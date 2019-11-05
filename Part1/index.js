c4c494cd206eb361dd8143a18187dd2bb1508cf6
Greg Kezos <greg.kezos@gmail.com>
2019-11-05 14:49:40 -0700

    the array in notes

--- Part1/index.js
+++ Part1/index.js
@@ -7,6 +7,12 @@ const play = id => {
   clickedBox.innerText = playerSpan.innerText
   board[id] = playerSpan.innerText
 
+/*
+ 0 1 2
+ 3 4 5
+ 6 7 8
+*/
+
   if ( board[0] !== undefined && board[0] === board[1] && board[0] === board[2]) {
   alert(`Congratulations, Player ${playerSpan.innerText} wins!`);
   return;
